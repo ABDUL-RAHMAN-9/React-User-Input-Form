ec597fc796491b8b8fb3a163b60ee5a1af2628d1
Abdul Rahman <abdulrahman161004@gmail.com>
2025-03-12 20:38:53 +0530

    Created Password field also and Use tailwind for styling

--- src/UserForm .jsx
+++ src/UserForm .jsx
@@ -5,41 +5,58 @@ function UserForm()
 {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
 
     const handleSumbit = (event) =>
     {
         event.preventDefault();
-        if (!name || !email)
+        if (!name || !email || !password)
         {
             alert("Please fill in all the fields before submitting.");
             return;
         }
-        console.log(`Name: ${name} , Email: ${email}`);
+        alert(`Name: ${name} \nEmail: ${email}\nPassword: ${password}`);
         setName('');
         setEmail('');
+        setPassword('');
 
     };
 
     return (
-        <div className=" min-h-screen border-2 m-4 p-5 rounded-3xl w-120 flex items-center justify-center">
-            <div className='text-center flex items-center justify-center'>
+        <div className=" min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-900 to-blue-600">
+            <div className='border-2 m-4 p-5 rounded-3xl w-120 h-120 text-center flex flex-col items-center justify-center shadow-2xl bg-gray-800'>
+                <h2 className='text-2xl text-white text-center mb-6'>USER LOGIN</h2>
                 <form onSubmit={handleSumbit}>
+                    {/* Name input */}
                     <input type="text"
                         placeholder='Enter Name:'
                         value={name}
-                        className='border rounded p-2 font-semibold w-full mb-5'
+                        className='border rounded p-4 font-semibold w-full mb-5 bg-gray-700 text-white focus:outline-none focus:ring-1'
                         onChange={(event) => setName(event.target.value)}
                     />
 
+                    {/* Passwrod Input */}
                     <input type="text"
                         placeholder='Enter Email:'
                         value={email}
-                        className='border rounded p-2 font-semibold w-full mb-5'
+                        className='border rounded p-4 font-semibold w-full mb-5 bg-gray-700 text-white focus:outline-none focus:ring-1'
                         onChange={(event) => setEmail(event.target.value)}
                     />
+                    {/* Passowrd input */}
+                    <input type="password"
+                        placeholder='Enter Password'
+                        value={password}
+                        className='border rounded p-4 font-semibold w-full mb-5 bg-gray-700 text-white focus:outline-none focus:ring-1'
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+
+
+                    {/* Submit Button */}
                     <button
                         type='submit'
-                        className='border rounded-xl py-2 px-4 hover:bg-zinc-600 text-white bg-zinc-500'
+                        className='w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600'
                     >Submit</button>
                 </form>
             </div>
